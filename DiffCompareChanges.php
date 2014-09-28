707a58a476ab162114d2a54499372c587234b97c
xy1 <mattscandale@hotmail.com>
2014-09-27 23:16:39 -0500

    Compare changes between 2 files

--- DiffCompareChanges.php
+++ DiffCompareChanges.php
@@ -5,8 +5,7 @@
  * Custom DIFF algorithm - compare changes between 2 files
  *
  * You've used the DIFF function in Linux or GitHub.  Ever try to write your own?  This was a fun problem-solving
- * exercise.  I spent about 8 hours on it.  When I get more time, I'll optimize its performance more.
- * I got the inspiration for this from a post that I saw online.
+ * exercise.  I spent about 8 hours on it.
  *
  * Phase 1 of 2
  *
@@ -31,116 +30,101 @@
  *
  */
 
-function HighlightDifferences( $oPageContent1, $oPageContent2 ) {
-        $l = strlen($oPageContent1);
+class Differences {
+	function HighlightDifferences($oPageContent1, $oPageContent2) {
+        	$l = strlen($oPageContent1);
+	
+        	$segments = array ();
+        	$segment_count = 0;
+	
+        	$i = 0;
+        	$j = 0;
+	
+        	$longest_start = -1;
+        	$longest_length = 0;
+        	for ($i = 0; $i <= $l; $i++ ) {
+	
+                	// skip over common sections
+                	$skip = 0;
+                	foreach ($segments as $s) {
+                        	$tmp_start = substr($s, 0, strpos($s,"for"));
+                        	$tmp_length = substr($s, strpos($s,"for") + 3);
+                        	if ($i >= $tmp_start && $i <= $tmp_start + $tmp_length) $skip = 1;
+                	}
+                	if ($skip == 1) continue;
+	
+                	$tmp_longest_start = 0;
+                	$tmp_longest_length = 0;
+                	for ($j = 1; $j <= ($l - $i); $j++ ) {
+	
+                        	// stop if reached common section
+                        	$stop = 0;
+                        	foreach ($segments as $s) {
+                                	$tmp_start = substr($s, 0, strpos($s,"for"));
+                                	$tmp_length = substr($s, strpos($s,"for") + 3);
+                                	if ($j >= $tmp_start && $j <= $tmp_start + $tmp_length) $stop = 1;
+                        	}
+	
+                        	$chunk = substr($oPageContent1, $i, $j );
+                        	if (strpos($oPageContent2,$chunk) !== FALSE) {
+                                	$tmp_longest_start = $i;
+                                	$tmp_longest_length = strlen($chunk) - 1;
+                                	if ($tmp_longest_length > $longest_length) {
+                                        	$longest_start = $tmp_longest_start;
+                                        	$longest_length = $tmp_longest_length;
+                                	}
+                                	if ($stop == 1) break;
+                        	}
+                        	else break;
+                	}
+	
+                	if ($longest_start < 0) break;
 
-        $segments = array ();
-        $segment_count = 0;
-
-        $i = 0;
-        $j = 0;
-
-        $longest_start = -1;
-        $longest_length = 0;
-        for ($i = 0; $i <= $l; $i++ ) {
-
-                // skip over common sections
-                $skip = 0;
-                foreach ( $segments as $s ) {
-                        $tmp_start = substr($s, 0, strpos($s,"for"));
-                        $tmp_length = substr($s, strpos($s,"for") + 3);
-                        if ( $i >= $tmp_start && $i <= $tmp_start + $tmp_length ) {
-                                $skip = 1;
-                        }
-                }
-                if ( $skip == 1 ) {
-                        continue;
-                }
-
-                $tmp_longest_start = 0;
-                $tmp_longest_length = 0;
-                for ($j = 1; $j <= ($l - $i); $j++ ) {
-
-                        // stop if reached common section
-                        $stop = 0;
-                        foreach ( $segments as $s ) {
-                                $tmp_start = substr($s, 0, strpos($s,"for"));
-                                $tmp_length = substr($s, strpos($s,"for") + 3);
-                                if ( $j >= $tmp_start && $j <= $tmp_start + $tmp_length ) {
-                                        $stop = 1;
-                                }
-                        }
-
-                        $chunk = substr($oPageContent1, $i, $j );
-                        if ( strpos($oPageContent2,$chunk) !== FALSE ) {
-                                $tmp_longest_start = $i;
-                                $tmp_longest_length = strlen($chunk) - 1;
-                                if ( $tmp_longest_length > $longest_length ) {
-                                        $longest_start = $tmp_longest_start;
-                                        $longest_length = $tmp_longest_length;
-                                }
-                                if ( $stop == 1 ) {
-                                        break;
-                                }
-                        }
-                        else {
-                                break;
-                        }
-                }
-
-                if ( $longest_start < 0 ) {
-                        break;
-                }
-
-                $segments[$segment_count] = $longest_start . "for" . $longest_length;
-                $segment_count++;
-        }
-
-        // parse content word-by-word and see which words (if any) have changes in them
-        $last_word_end = -1;
-        for ($i = 0; $i <= $l; $i++ ) {
-                if ( substr($oPageContent1, $i, 1 ) == ' ' || $i == $l ) { // end of word or end of string
-                        $word_start = $last_word_end + 1;
-                        $word_end = $i - 1;
-                        $word_length = $word_end - $word_start + 1;
-                        $word = substr($oPageContent1, $word_start, $word_length);
-                        $last_word_end = $i;
-                        $word_is_ok = 0;
-                        foreach ( $segments as $s ) {
-                                $tmp_start = substr($s, 0, strpos($s,"for"));
-                                $tmp_length = substr($s, strpos($s,"for") + 3);
-                                $tmp_end = $tmp_start + $tmp_length;
-                                if ( $word_start >= $tmp_start && $word_end <= $tmp_end ) {
-                                        $word_is_ok = 1;
-                                }
-                        }
-        
-                        if ( $word_is_ok == 0 && trim($word) > '' ) {
-                                $oPageContent1 = substr($oPageContent1, 0, $word_start) .
-                                        strtoupper( substr($oPageContent1, $word_start, $word_length) ) .
-                                        substr($oPageContent1, $word_end + 1);
-                        }
-                }
-        }
-
-        return $oPageContent1;
+                	$segments[$segment_count] = $longest_start . "for" . $longest_length;
+                	$segment_count++;
+        	}
+	
+        	// parse content word-by-word and see which words (if any) have changes in them
+        	$last_word_end = -1;
+        	for ($i = 0; $i <= $l; $i++ ) {
+                	if (substr($oPageContent1, $i, 1 ) == ' ' || $i == $l) { // end of word or end of string
+                        	$word_start = $last_word_end + 1;
+                        	$word_end = $i - 1;
+                        	$word_length = $word_end - $word_start + 1;
+                        	$word = substr($oPageContent1, $word_start, $word_length);
+                        	$last_word_end = $i;
+                        	$word_is_ok = 0;
+                        	foreach ($segments as $s) {
+                                	$tmp_start = substr($s, 0, strpos($s,"for"));
+                                	$tmp_length = substr($s, strpos($s,"for") + 3);
+                                	$tmp_end = $tmp_start + $tmp_length;
+                                	if ($word_start >= $tmp_start && $word_end <= $tmp_end) $word_is_ok = 1;
+                        	}
+       	 
+                        	if ($word_is_ok == 0 && trim($word) > '') {
+                                	$oPageContent1 = substr($oPageContent1, 0, $word_start) .
+                                        	strtoupper(substr($oPageContent1, $word_start, $word_length)) .
+                                        	substr($oPageContent1, $word_end + 1);
+                        	}
+                	}
+        	}
+	
+        	return $oPageContent1;
+	}
 }
+
+$differences = new Differences();
 
 $oPageContent1 = "the apple beta circus dog";
 $oPageContent2 = "the pear beta circus cat";
 
-$oPageContent1 = "Smart Investing is a new consumer education program to provide investors with necessary tools " .
-    "to make informed decisions.";
-$oPageContent2 = "Smart Investing is a new corporate education program to provide investors with necessary tools " .
-    "to make informed decisions.";
-
 echo "original 1: " . $oPageContent1 . "\n";
 echo "original 2: " . $oPageContent2 . "\n";
 
-$oPageContent1 = HighlightDifferences( $oPageContent1, $oPageContent2 );
-$oPageContent2 = HighlightDifferences( $oPageContent2, $oPageContent1 );
+$o1 = $differences->HighlightDifferences($oPageContent1, $oPageContent2);
+$o2 = $differences->HighlightDifferences($oPageContent2, $oPageContent1);
 
-echo "modified 1: " . $oPageContent1 . "\n";
-echo "modified 2: " . $oPageContent2 . "\n";
+echo "modified 1: " . $o1 . "\n";
+echo "modified 2: " . $o2 . "\n";
 
 ?>
